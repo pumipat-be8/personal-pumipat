3dc967a7d82d9c087b95afca301f7fb96ddb8459
Beryl8Plus <charnun.t@beryl8.com>
2016-07-11 14:44:06 +0700

    Automated Sync 2016-07-11_0244 PM

--- commit.bat
+++ commit.bat
@@ -1,8 +1,9 @@
-rem @echo off
+@echo off
 for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+for /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 rem add all to stage
 git add *
 rem commit to stage
-git commit -m "Automated Sync %mydate%"
+git commit -m "Automated Sync %mydate%_%mytime%"
 rem Push them up to the repo
 git push -u origin master
